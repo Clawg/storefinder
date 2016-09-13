6a9526dbd4ad8766f96b3f087e6fd152a9e10c71
Charlie Law <Charlie.Law@lottonz.co.nz>
2016-09-13 16:40:45 +1200

    merged funds

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import constants from './constants'
 import test from './test'
 import actions from './actions'
 
-export {
-  constants,
-  test,
-  actions
+const StoreFinder = {
+    constants: constants, 
+    test:  test,
+    actions: actions
 }
+
+export default StoreFinder
+
+// export default StoreFinder {
+//   constants,
+//   test,
+//   actions
+// }
